5acb3396cab8c3d466b8c08a326ef4c73a1ab52e
Linsjosephmathew <74895209+Linsjosephmathew@users.noreply.github.com>
2022-05-21 15:29:10 +0530

    Update Typing speed.py

--- Typing speed.py
+++ Typing speed.py
@@ -1,8 +1,8 @@
 from pynput.keyboard import Key, Listener
 import datetime
 
-sentence = " An Ethical Hacker is a skilled professional who has excellent technical knowledge and skills and knows how to identify and exploit vulnerabilities in target systems. He works with the permission of the owners of systems. An ethical Hacker must comply with the rules of the target organization or owner and the law of the land and their aim is to assess the security posture of a target organization/system."
-print("Type this as fast as you can!")
+sentence = "He found himself sitting at his computer, typing whatever came to mind. He was on an application entitled Typing Test. This application tested how fast you were \nat typing. So he typed. He was currently typing about himself typing, which is odd in a way. He was now describing about how he was typing about himself typing.\n"
+print("\nType this as fast as you can!\n")
 print(sentence)
 correct, incorrect = 0, 0
 current_index = 0
